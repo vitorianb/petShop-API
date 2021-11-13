6914b5375ed6ad1368708a6679bccf3af91dd0fd
Vitória Bernardino <vitorianbernardino@gmail.com>
2021-11-13 10:06:02 -0300

    padrão mvc

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
-const app = express()
+const customExpress = require('./config/customExpress')
+const app = customExpress
 app.listen(3000, ()=> console.log('servidor rodando na porta 3000'))
-app.get('/atendimentos', (req, res)=> res.send('Você está na rota de atendimentos e está realizando um get'))
+
