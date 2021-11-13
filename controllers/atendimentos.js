073c6954ff04a8ad197baf8b64030c87357fe653
Vitória Bernardino <vitorianbernardino@gmail.com>
2021-11-13 10:35:45 -0300

    formato de requisição e recebimentos dos dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,7 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res)=> res.send('Você está na rota de atendimentos e está realizando um get'))
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos realizando um post')
+    })
 }
